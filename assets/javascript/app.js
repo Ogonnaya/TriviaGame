f91e8ffa01b245f0798afdbf6128247f000dc610
Ogonnaya <hello@ogonnaya.com>
2019-10-14 15:37:19 -0500

    updated html, css & js - updated code for timer and button styles.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,69 +1,156 @@
+// $(document).ready(function () {
 var triviaQuestions = [
   {
     question: "What is the only U.S. State that only borders one other?",
-    option_one: "Rhode Island",
-    option_two: "Maine", // Correct answer
-    option_three: "Washington",
-    option_four: "Florida"
+    choices: [
+      "Rhode Island",
+      "Maine" /* Correct answer */,
+      "Washington",
+      "Florida"
+    ],
+    answer: 1
+    // photo: "assets/images/main.gif"
   },
   {
     question:
       "Name the number that is three more than one-fifth of one-tenth of one-half of 5,000.",
-    option_one: 503,
-    option_two: 103,
-    option_three: 53, // Correct answer
-    option_four: 108
+    choices: ["503", "103", "53" /* Correct answer */, "108"],
+    answer: 2
   },
   {
     question: "What's the oldest continuously inhabited city in the world?",
-    option_one: "Istanbul, Turkey",
-    option_two: "Athens, Greece",
-    option_three: "Jerusalem",
-    option_four: "Damascus, Syria" // Correct answer
+    choices: [
+      "Istanbul, Turkey",
+      "Athens, Greece",
+      "Jerusalem",
+      "Damascus, Syria" /* Correct answer */
+    ],
+    answer: 3
   },
   {
     question: "What is the only U.S. State with a Spanish motto?",
-    option_one: "Idaho",
-    option_two: "California",
-    option_three: "Montana", // Correct answer
-    option_four: "Arizona"
+    choices: ["Idaho", "California", "Montana" /* Correct answer */, "Arizona"],
+    answer: 3
   },
   {
     question: "Which of these is a perfect number?",
-    option_one: 1,
-    option_two: 6, // Correct answer
-    option_three: 7,
-    option_four: 21
+    choices: ["1", "6" /* Correct answer */, "7", "21"],
+    answer: 2
   },
   {
     question:
       "What is the maximum number of words that a single dog can understand?",
-    option_one: 60,
-    option_two: 90,
-    option_three: 120,
-    option_four: 200 // Correct answer
+    choices: ["60", "90", "120", "200"],
+    answer: 3
   },
   {
     question:
       "What comic book character did Michael Jackson want to play so much that he considered buying Marvel Comics in the 1990s?",
-    option_one: "Wolverine",
-    option_two: "The Hulk",
-    option_three: "Professor X",
-    option_four: "Spider-Man" // Correct answer
+    choices: [
+      "Spider-Man" /* Correct answer*/,
+      "Wolverine",
+      "The Hulk",
+      "Professor X"
+    ],
+    answer: 0
   },
   {
     question:
       "What color are the not-so-golden McDonald’s arches at the McDonald’s in Sedona, Arizona?",
-    option_one: "Red",
-    option_two: "Black",
-    option_three: "Turquoise", // Correct answer
-    option_four: "Purple"
+    choices: ["Red", "Black", "Turquoise", "Purple"],
+    answer: 2
   },
   {
     question: "What was the original name of the band Led Zepplin",
-    option_one: "The Rain",
-    option_two: "Kara's Flowers",
-    option_three: "The New Yardbirds", // Correct answer
-    option_four: "Wicked Lester"
+    choices: [
+      "The Rain",
+      "Kara's Flowers",
+      "The New Yardbirds" /* Correct answer*/,
+      "Wicked Lester"
+    ],
+    answer: 2
+  },
+  {
+    question:
+      "Which original Winnie the Pooh voice actor attempted to create an artificial heart?",
+    choices: ["Tigger" /* Correct answer */, "Pooh", "Piglet", "Eeyore"],
+    answer: 0
   }
 ];
+
+// console.log(triviaQuestions[1].choices[3]);
+
+var correctAnswers = 0; // Total number of correct answers will be shown at the end of the game
+var wrongAnswers = 0; // Total number of wrong answers will be shown at the end of the game or when timer runs out
+var timer = 89; // Total amount of time the player has to answer all the questions
+var intervalID;
+var userGuess = "";
+var running = false;
+var qCount = triviaQuestions.length;
+var pick;
+var index;
+var newArray = [];
+var holder = [];
+
+// At the begining of the game...
+// Show timer at maximum seconds remaining
+// Show instructions
+// Show start button
+
+// ********** START GAME **********
+
+// When player clicks start...
+$("#start").on("click", function() {
+  $("#start").hide();
+  showQuestion();
+  startTimer();
+  for (var i = 0; i < triviaQuestions.length; i++) {
+    holder.push(triviaQuestions[i]);
+  }
+});
+
+// ********** TIMER **********
+
+// Start timer
+function startTimer() {
+  if (!running) {
+    intervalId = setInterval(countdown, 1000);
+    running = true;
+  }
+}
+// Countdown
+function countdown() {
+  $("#timer").html(timer);
+  timer--;
+
+  // Stop game when countdown reaches 0
+  if (timer === -1) {
+    stop();
+  }
+}
+
+// Stop timer
+function stop() {
+  running = false;
+  clearInterval(intervalId);
+}
+
+// ********** QUESTION FUNCTIONS **********
+
+// Show random question
+function showQuestion() {
+  index = Math.floor(Math.random() * triviaQuestions.length);
+  pick = triviaQuestions[index];
+
+  //iterate through answer array and display
+  $("#questions").html(pick.question);
+  for (var i = 0; i < pick.choices.length; i++) {
+    var userChoice = $("<div>");
+    userChoice.addClass("btn btn-outline-dark btn-lg");
+    userChoice.html(pick.choices[i]);
+    //assign array position to it so can check answer
+    userChoice.attr("data-guessvalue", i);
+    $("#choices").append(userChoice);
+    //		}
+  }
+}
