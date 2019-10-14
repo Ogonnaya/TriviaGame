2b184087c97d579553a186da396846a7cc83e2d1
Ogonnaya <hello@ogonnaya.com>
2019-10-13 23:00:20 -0500

    updated css, javascript and html

---
+++ assets/javascript/app.js
@@ -0,0 +1,69 @@
+var triviaQuestions = [
+  {
+    question: "What is the only U.S. State that only borders one other?",
+    option_one: "Rhode Island",
+    option_two: "Maine", // Correct answer
+    option_three: "Washington",
+    option_four: "Florida"
+  },
+  {
+    question:
+      "Name the number that is three more than one-fifth of one-tenth of one-half of 5,000.",
+    option_one: 503,
+    option_two: 103,
+    option_three: 53, // Correct answer
+    option_four: 108
+  },
+  {
+    question: "What's the oldest continuously inhabited city in the world?",
+    option_one: "Istanbul, Turkey",
+    option_two: "Athens, Greece",
+    option_three: "Jerusalem",
+    option_four: "Damascus, Syria" // Correct answer
+  },
+  {
+    question: "What is the only U.S. State with a Spanish motto?",
+    option_one: "Idaho",
+    option_two: "California",
+    option_three: "Montana", // Correct answer
+    option_four: "Arizona"
+  },
+  {
+    question: "Which of these is a perfect number?",
+    option_one: 1,
+    option_two: 6, // Correct answer
+    option_three: 7,
+    option_four: 21
+  },
+  {
+    question:
+      "What is the maximum number of words that a single dog can understand?",
+    option_one: 60,
+    option_two: 90,
+    option_three: 120,
+    option_four: 200 // Correct answer
+  },
+  {
+    question:
+      "What comic book character did Michael Jackson want to play so much that he considered buying Marvel Comics in the 1990s?",
+    option_one: "Wolverine",
+    option_two: "The Hulk",
+    option_three: "Professor X",
+    option_four: "Spider-Man" // Correct answer
+  },
+  {
+    question:
+      "What color are the not-so-golden McDonald’s arches at the McDonald’s in Sedona, Arizona?",
+    option_one: "Red",
+    option_two: "Black",
+    option_three: "Turquoise", // Correct answer
+    option_four: "Purple"
+  },
+  {
+    question: "What was the original name of the band Led Zepplin",
+    option_one: "The Rain",
+    option_two: "Kara's Flowers",
+    option_three: "The New Yardbirds", // Correct answer
+    option_four: "Wicked Lester"
+  }
+];
